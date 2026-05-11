357d203444b5429d1181483da9cb0bfc5536f669
ibrahimboateng740-jpg <ibrahimboateng740@gmail.com>
2026-05-11 16:59:31 +0000

    Update PortalApplication.java

--- src/main/java/com/nexus/portal/PortalApplication.java
+++ src/main/java/com/nexus/portal/PortalApplication.java
@@ -3,9 +3,12 @@ package com.nexus.portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 
-// Adding (exclude = ...) turns off the login page so you can see your site
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = { 
+    SecurityAutoConfiguration.class, 
+    ManagementWebSecurityAutoConfiguration.class 
+})
 public class PortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
