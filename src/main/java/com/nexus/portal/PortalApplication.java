d6f2b0651eef3534dfd9014404cc91bbb4f46b84
ibrahimboateng740-jpg <ibrahimboateng740@gmail.com>
2026-05-11 17:08:52 +0000

    Update PortalApplication.java

--- src/main/java/com/nexus/portal/PortalApplication.java
+++ src/main/java/com/nexus/portal/PortalApplication.java
@@ -2,13 +2,8 @@ package com.nexus.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { 
-    SecurityAutoConfiguration.class, 
-    ManagementWebSecurityAutoConfiguration.class 
-})
+@SpringBootApplication
 public class PortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
