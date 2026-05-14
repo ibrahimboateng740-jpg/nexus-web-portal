9ef90e5f7ffccd880b1c97fd600f426fc9f35fb4
ibrahimboateng740-jpg <ibrahimboateng740@gmail.com>
2026-05-14 06:39:19 +0000

    Create TheNexusWebApplication.java

---
+++ src/main/java/com/nexus/TheNexusWebApplication.java
@@ -0,0 +1,11 @@
+package com.nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheNexusWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TheNexusWebApplication.class, args);
+    }
+}
