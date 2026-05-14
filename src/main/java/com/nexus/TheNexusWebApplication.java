5cf370addab208b1db9fb5f4cc5d826b29042e02
ibrahimboateng740-jpg <ibrahimboateng740@gmail.com>
2026-05-14 06:56:39 +0000

    Update TheNexusWebApplication.java

--- src/main/java/com/nexus/TheNexusWebApplication.java
+++ src/main/java/com/nexus/TheNexusWebApplication.java
@@ -1,11 +1,13 @@
-package com.nexus;
+package com.nexus; // Ensure this matches your folder path exactly
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TheNexusWebApplication {
+
     public static void main(String[] args) {
+        // This is the power switch that starts TheNexusWeb
         SpringApplication.run(TheNexusWebApplication.class, args);
     }
 }
