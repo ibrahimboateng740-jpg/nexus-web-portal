8ae5e65ca06c5e83f86157239a622dae963654ec
ibrahimboateng740-jpg <ibrahimboateng740@gmail.com>
2026-05-14 19:52:16 +0000

    Update WebController.java

--- src/main/java/com/nexus/WebController.java
+++ src/main/java/com/nexus/WebController.java
@@ -5,29 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
-
-    @GetMapping("/")
-    public String home() {
-        return "index";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-
-    @GetMapping("/social")
-    public String social() {
-        return "social";
-    }
-
-    @GetMapping("/privacy")
-    public String privacy() {
-        return "privacy";
-    }
-    
-    @GetMapping("/news")
-    public String news() {
-        return "news";
-    }
+    @GetMapping("/") public String home() { return "index"; }
+    @GetMapping("/social") public String social() { return "social"; }
+    @GetMapping("/login") public String login() { return "login"; }
+    @GetMapping("/news") public String news() { return "news"; }
+    @GetMapping("/privacy") public String privacy() { return "privacy"; }
 }
