63a8ca9cf38dff2338970b54d8c43f5261ec2672
ibrahimboateng740-jpg <ibrahimboateng740@gmail.com>
2026-05-14 01:55:21 +0000

    Create WebController.java

---
+++ src/main/src/main/java/com/nexus/WebController.java
@@ -0,0 +1,18 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String home() { return "index"; }
+
+    @GetMapping("/login")
+    public String login() { return "login"; }
+
+    @GetMapping("/signup")
+    public String signup() { return "signup"; }
+
+    @GetMapping("/search")
+    public String search() { return "search"; }
+}
