bfd745821af8fdf877ebdb1b8eee692a3c2b74a5
ibrahimboateng740-jpg <ibrahimboateng740@gmail.com>
2026-05-16 21:33:35 +0000

    Add UserProfileRepository interface for user profiles

---
+++ src/main/java/com/thenexusweb/repository/UserProfileRepository.java
@@ -0,0 +1,9 @@
+package com.thenexusweb.repository;
+
+import com.thenexusweb.model.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+    Optional<UserProfile> findByUsername(String username);
+}
